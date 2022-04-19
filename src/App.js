ac0d7b0a1267630c8189cc677b4857459e4ad7ac
taksas <takuminakayama4869@gmail.com>
2022-04-19 22:34:27 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import Button from '@mui/material/Button';
+
+
+
+const query = gql`
+  {
+    organization(login: "apollographql") {
+      repositories(first: 5, isFork: false) {
+        nodes {
+          id
+          name
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`;
+
+const App = () => (
+  <Query query={query}>
+    {({ loading, data }) => {
+      if (loading) return <p>Loading...</p>;
+
+      
+      const repositories = data.organization.repositories.nodes;
+
+      return (
+        <ul>
+          <Button variant="contained" color="primary">
+            Hello World
+         </Button>
+          {repositories.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.url}>{repo.name}</a>
+              <button>{repo.stargazers.totalCount} Star</button>
+            </li>
+          ))}
+        </ul>
+      );
+    }}
+  </Query>
+);
+
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from 'react-apollo';
+
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${
+          process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+        }`
+      }
+    });
+  }
+});
+
+
+
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+  );
+
