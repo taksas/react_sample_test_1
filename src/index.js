cd19774c81ee9d8bc0b8caa74416600d53760c73
taksas <takuminakayama4869@gmail.com>
2022-04-20 10:59:56 +0900

    Get started with Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,30 +1,68 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from 'react-apollo';
+// import App from './App';
+import Button from '@mui/material/Button';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
 
 const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  request: operation => {
-    operation.setContext({
-      headers: {
-        authorization: `Bearer ${
-          process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
-        }`
-      }
-    });
-  }
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
 });
 
 
+  
+const EXCHANGE_RATES = gql`
+query GetExchangeRates {
+  rates(currency: "USD") {
+    currency
+    rate
+  }
+}
+`;
 
-  ReactDOM.render(
+function ExchangeRates() {
+const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+if (loading) return <p>Loading...</p>;
+if (error) return <p>Error :(</p>;
+
+return data.rates.map(({ currency, rate }) => (
+  <div key={currency}>
+    <p>
+      {currency}: {rate}
+    </p>
+  </div>
+));
+}
+
+function App() {
+  return (
+    <ul>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates />
+    
+      <Button variant="contained" color="primary">
+            Hello World
+         </Button>
+
+    </ul>
+  );
+}
+
+
+
+  render(
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>,
-    document.getElementById('root')
+    document.getElementById('root'),
   );
 
